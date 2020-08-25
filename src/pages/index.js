0b297f73ebc638393f3699a7db46bcde118b54bc
Chris Cosentino <chriscosentino@hotmail.com>
2020-08-25 19:55:17 -0400

    added meta

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,6 +9,29 @@ const IndexPage = () => {
     <div className="container">
       <Helmet>
         <title>Chris Cosentino</title>
+        <meta name="title" content="Chris Cosentino" />
+        <meta
+          name="description"
+          content="Chris Cosentino's project portfolio website. A Software Developer."
+        />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://www.thecosentino.com/" />
+        <meta property="og:title" content="Chris Cosentino" />
+        <meta
+          property="og:description"
+          content="Chris Cosentino's project portfolio website. A Software Developer."
+        />
+        <meta property="og:image" content="" />
+
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://www.thecosentino.com/" />
+        <meta property="twitter:title" content="Chris Cosentino" />
+        <meta
+          property="twitter:description"
+          content="Chris Cosentino's project portfolio website. A Software Developer."
+        />
+        <meta property="twitter:image" content="" />
       </Helmet>
       <Landing />
     </div>
