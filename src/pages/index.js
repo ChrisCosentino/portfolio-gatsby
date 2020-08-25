6c22d280b0f99478d5e6cfb831d5f7abc4e96b0d
Chris Cosentino <chriscosentino@hotmail.com>
2020-08-25 16:26:07 -0400

    fixed sizing

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import React from "react"
+import Helmet from "react-helmet"
 import Landing from "../components/landing"
 
 import "../styles/styles.css"
@@ -6,6 +7,9 @@ import "../styles/styles.css"
 const IndexPage = () => {
   return (
     <div className="container">
+      <Helmet>
+        <title>Chris Cosentino</title>
+      </Helmet>
       <Landing />
     </div>
   )
